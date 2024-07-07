426ef016337080cf9a38f92c154f3771768c0ab9
netanel97 <netanelhabas@gmail.com>
2024-07-07 15:33:53 +0300

    Adding Dockerfile,compose.yaml and SQLAlchemySingleton instance db

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Upgrade pip
+RUN pip install --upgrade pip
+
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc \
+    && pip install psycopg2
+
+# Install any needed packages specified in requirements.txt
+RUN pip install -r requirements.txt
+
+
+RUN apt-get update
+
+EXPOSE 5000
+
+
+CMD ["python", "./application.py"]
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from dal.database import db  # Import SQLAlchemySingleton instance
 
-app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_app():
+    app = Flask(__name__)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123123@host.docker.internal:5432/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123123@postgres:5432/postgres'
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)  # Initialize SQLAlchemy with the app
+    with app.app_context():
+        # Create database tables based on models
+        db.create_all()
+        # Register blueprints
+        # app.register_blueprint(user_bp, url_prefix='/users')
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0")
