4064f50be60d20b50a203f6f6db0b82ce2a087f2
netanel97 <netanelhabas@gmail.com>
2024-07-08 15:45:59 +0300

    Removed alembic script(it will explain in the readme file). add env file and deleted the passwords from github

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 from dal.database import db  # Import SQLAlchemySingleton instance
 from controllers.question_controller import question_bp
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123123@db/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)  # Initialize SQLAlchemy with the app
     migrate = Migrate(app, db)
@@ -19,5 +23,5 @@ def create_app():
 
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, host="0.0.0.0")
+    flask_app = create_app()
+    flask_app.run(debug=True, host="0.0.0.0")
